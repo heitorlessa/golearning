812467218218fe289e0b14b80b345212feb1d97c
Heitor Lessa <heitor.lessa@hotmail.com>
2015-07-24 21:59:47 +0100

    [+] Added 'run' method that takes care of message handling and can be now used as a goroutine safely

--- exercises/chat_websocket/room.go
+++ exercises/chat_websocket/room.go
@@ -14,3 +14,41 @@ type room struct {
 	// holds all clients in this room
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		// select ensures that code run one block of code at a time
+		select {
+
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+
+		// forward message to all clients if a message is ever sent to 'forward' channel
+		case msg := <-r.forward:
+			// goes through all clients (i :=0; i < # of clients; i++ -- short version)
+			for client := range r.clients {
+				select {
+
+				// sends message on 'send' channel on each client as it will be picked up by 'write' method
+				// in which will write down the message to browser's web socket
+				case client.send <- msg:
+					// send message
+
+				// in case client doesnt accept the message, removes the client and tide things up
+				default:
+					// failed to send message
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+
+		}
+	}
+
+}
