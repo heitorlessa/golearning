bfc5c705fc1bbaf0c49c48cc647dd39824e15118
Heitor Lessa <heitor.lessa@hotmail.com>
2015-07-24 21:30:12 +0100

    [+] Added Join and Leave channel to avoid sending/reading message from the same channel to avoid accessing the same data at the same time [+] Added 'Clients' map['client name'] that will hold clients

--- exercises/chat_websocket/room.go
+++ exercises/chat_websocket/room.go
@@ -1,7 +1,16 @@
 package main
 
 type room struct {
-	
+
 	// Channel 'Forward' will be holding incoming messages
 	forward chan []byte
+
+	// Clients wishing to join the room
+	join chan []byte
+
+	// clients wishing to leave the room
+	leave chan []byte
+
+	// holds all clients in this room
+	clients map[*client]bool
 }
